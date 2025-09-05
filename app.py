25450d904a3d87b99e53c35253d567ec66a677df
Aman Churiwal <amanchuriwal22@gmail.com>
2025-09-06 00:44:33 +0530

    Video Downloader Setup for Hosting

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from tasks import download_video_task
+import boto3
+
+app = Flask(__name__)
+S3_BUCKET_NAME = "video-downloader-bucket"
+
+@app.route('/download', methods=['POST'])
+def start_download():
+    data = request.get_json()
+    if not data or 'url' not in data:
+        return jsonify({'error': 'URL is required in the JSON body.'}), 400
+    task = download_video_task.delay(data['url'])
+    return jsonify({'message': 'Download task has been accepted.', 'task_id': task.id}), 202
+
+@app.route('/status/<task_id>', methods=['GET'])
+def get_task_status(task_id):
+    task_result = download_video_task.AsyncResult(task_id)
+    response = {'task_id': task_id, 'status': task_result.state}
+
+    if task_result.state == 'SUCCESS':
+        s3_key = task_result.result.get('s3_key')
+        # Generate a temporary, secure link to the file in S3
+        s3_client = boto3.client('s3')
+        download_url = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': S3_BUCKET_NAME, 'Key': s3_key},
+            ExpiresIn=3600  # Link is valid for 1 hour
+        )
+        response['download_url'] = download_url
+    elif task_result.state == 'FAILURE':
+        response['result'] = str(task_result.info) # Include error info
+
+    return jsonify(response)
---
+++ tasks.py
@@ -0,0 +1,37 @@
+import os
+import yt_dlp
+import boto3
+from celery import Celery
+
+# Use your ElastiCache Primary Endpoint here
+REDIS_URL = "video-downloader-redis-avlitx.serverless.aps1.cache.amazonaws.com:6379"
+S3_BUCKET_NAME = "video-downloader-bucket"
+
+celery_app = Celery('tasks', broker=REDIS_URL, backend=REDIS_URL)
+
+@celery_app.task(name='tasks.download_video')
+def download_video_task(video_url):
+    # We will download the file to a temporary location on the server
+    temp_dir = "/tmp" 
+    ydl_opts = {
+        'outtmpl': os.path.join(temp_dir, '%(id)s.%(ext)s'),
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(video_url, download=True)
+            # Get the path of the downloaded file
+            downloaded_file_path = ydl.prepare_filename(info)
+            file_name = os.path.basename(downloaded_file_path)
+
+        # Upload the file from the temporary location to S3
+        s3_client = boto3.client('s3')
+        s3_client.upload_file(downloaded_file_path, S3_BUCKET_NAME, file_name)
+        
+        # Clean up the temporary file
+        os.remove(downloaded_file_path)
+        
+        # Return the S3 file key so the API can generate a download link
+        return {'status': 'Success', 's3_key': file_name}
+    except Exception as e:
+        raise e
