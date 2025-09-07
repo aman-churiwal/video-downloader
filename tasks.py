5c4c7499f3a08723cf1eadbc7a607c24e3a90b2c
Aman Churiwal <amanchuriwal22@gmail.com>
2025-09-08 00:56:38 +0530

    redis ssl issue resolution

--- tasks.py
+++ tasks.py
@@ -10,6 +10,8 @@ S3_BUCKET_NAME = "video-downloader-bucket"
 celery_app = Celery('tasks', broker=REDIS_URL, backend=REDIS_URL)
 
 celery_app.conf.update(
+    task_routes = {'tasks.*': {'queue': '{celery}-queue'}},
+
     worker_direct=True,
     broker_transport_options={
         'fanout_prefix': True,
